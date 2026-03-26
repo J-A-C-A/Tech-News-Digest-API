cbca983de2915d2312a13e14372a2b27b47cf56f
Jan Konczal <JanKonczal33@wp.pl>
2026-03-26 22:17:30 +0100

    dodanie funkcji sync_articles(), która pobiera artykuł, z api i zapisuje w lokalnej bazie. dodanie funkcji asynchronicznej lifespan, która przy uruchomieniu aplikacji tworzy obiekt scheduler, przypisuje mu zadanie i uruchamia. A przy zakończeniu działania aplikacji wyłącza obiekt scheduler

---
+++ main.py
@@ -0,0 +1,22 @@
+from external_db import fetch_articles
+from internal_db import save_article
+from apscheduler.schedulers.background import BackgroundScheduler
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+
+def sync_articles():
+    query = "technology"
+    articles = fetch_articles(query)
+
+    for article in articles:
+        save_article(title=article["title"] ,summary=article["description"],date=article["publishedAt"].split('T')[0],author=article["author"],source=article["source"]["name"] ,url= article["url"])
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=sync_articles,trigger="interval",minutes=60)
+    scheduler.start()
+    yield
+    scheduler.shutdown()
+app = FastAPI(lifespan=lifespan)
