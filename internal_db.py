7e8c067d6af059d47c999f62657aea0b50406a39
Jan Konczal <JanKonczal33@wp.pl>
2026-03-26 19:10:04 +0100

    Dodanie funkcji save_article(), która dodaje rekordy do bazy

--- internal_db.py
+++ internal_db.py
@@ -11,3 +11,15 @@ def get_connection():
     mydb = mysql.connector.connect(host=host, user=user, password=password, database=name)
     return mydb
 
+def save_article(title, summary, date, author, source, url):
+    db = get_connection()
+    cursor = db.cursor()
+    query = "INSERT INTO article (title,summary,date,author,source,url) VALUES (%s,%s,%s,%s,%s,%s);"
+    try:
+        cursor.execute(query, (title, summary, date, author, source, url))
+        db.commit()
+    except mysql.connector.Error as error:
+        print(error)
+    finally:
+        cursor.close()
+        db.close()
