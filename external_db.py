95278b10b2cf70fb751838f86d0ee9b62bde4542
Jan Konczal <JanKonczal33@wp.pl>
2026-03-31 20:11:52 +0200

    w main dodanie logów: ile artykułów pobrano z api, ile dodano nowych do bazy i ile odrzucono duplikatów, w external_db zmiana działania funkcji fetch_articles()

--- external_db.py
+++ external_db.py
@@ -2,9 +2,14 @@ import os
 import requests
 from dotenv import load_dotenv
 
-def fetch_articles(query):
+def fetch_articles():
     load_dotenv()
     key = os.getenv("API_KEY")
-    articles = requests.get(url="https://newsapi.org/v2/top-headlines",params={"q":query,"apiKey":key})
-    return articles.json()["articles"]
+    response = requests.get(url="https://newsapi.org/v2/top-headlines",params={"category":"technology","apiKey":key,"language":"en"})
+    data = response.json()
+    if data["status"] != "ok":
+        print("API doesn't work", data.get("message"))
+    if data["totalResults"] > 0:
+        print("There are some articles")
+    return data.get("articles",[])
 
--- main.py
+++ main.py
@@ -4,13 +4,27 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from endpoints import router
+import mysql.connector
+from mysql.connector import errorcode
 
 def sync_articles():
-    query = "technology"
-    articles = fetch_articles(query)
-
+    articles = fetch_articles()
+    total_number = len(articles)
+    number_of_saved = 0
+    number_of_skipped = 0
     for article in articles:
-        save_article(title=article["title"] ,summary=article["description"],date=article["publishedAt"].split('T')[0],author=article["author"],source=article["source"]["name"] ,url= article["url"])
+        try:
+            save_article(title=article["title"] ,summary=article["description"],date=article["publishedAt"].split('T')[0],author=article["author"],source=article["source"]["name"] ,url= article["url"])
+            number_of_saved += 1
+        except mysql.connector.Error as error:
+            if error.errno == errorcode.ER_DUP_ENTRY:
+                number_of_skipped += 1
+            else:
+                raise
+    print(f"Fetched {total_number} articles")
+    print(f"Save {number_of_saved} new articles")
+    print(f"Skipped {number_of_skipped} duplicates")
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
